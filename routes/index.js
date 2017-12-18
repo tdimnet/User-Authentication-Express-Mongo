34fb5769f247a845c990a1cd0b8acec4967da442
tdimnet <thomas.dimnet@gmail.com>
2017-12-18 12:10:19 +0100

    Add middleware for loggin users

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var mid = require('../middleware');
 
 
 // GET /profile
@@ -43,7 +44,7 @@ router.get('/logout', function(req, res, next) {
 });
 
 // GET /login
-router.get('/login', function(req, res, next) {
+router.get('/login', mid.loggedOut, function(req, res, next) {
   return res.render('login', { title: 'Log In' });
 });
 
@@ -72,7 +73,7 @@ router.post('/login', function(req, res, next) {
 });
 
 // GET /register
-router.get('/register', function(req, res, next) {
+router.get('/register', mid.loggedOut, function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
 });
 
